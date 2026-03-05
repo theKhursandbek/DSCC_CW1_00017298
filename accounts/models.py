33c1b9dd76a40f0729a3394eb9977b91fd1b4431
theKhursandbek <xsaidov@students.wiut.uz>
2026-03-05 05:47:29 +0500

    chore: clean up project - remove unused files, boilerplate comments, fix typos

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# Create your models here.
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
